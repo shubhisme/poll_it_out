a32c0e8d3c772a490f0f81e62e64fdfbe61fcb33
shubh solanki <145123927+shubhisme@users.noreply.github.com>
2026-04-19 17:54:58 +0530

    sso change

--- src/app/(auth)/signin/sso-callback/[[...index]]/page.tsx
+++ src/app/(auth)/signin/sso-callback/[[...index]]/page.tsx
@@ -1,43 +1,8 @@
-// "use client";
-
-// import { useEffect } from "react";
-// import { useClerk } from "@clerk/nextjs";
-
-// export default function SsoCallback() {
-//   const { handleRedirectCallback } = useClerk();
-
-//   useEffect(() => {
-
-//     // Clerk will finish the OAuth handshake here
-//     handleRedirectCallback().then(() => {
-//         // console.log(e);
-//         window.location.href = "/dashboard"; 
-//       })
-//       .catch((err) => {
-//         console.error("OAuth error:", err);
-//       });
-//   }, [handleRedirectCallback]);
-
-//   return <p>Finishing sign-in…</p>;
-// }
-
-// src/app/(auth)/signin/sso-callback/[[...index]]/page.tsx
 "use client";
 
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 
 export default function Page() {
-//   const router = useRouter();
-//   const [returnUrl, setReturnUrl] = useState("/dashboard");
-
-//   useEffect(() => {
-//     const stored = sessionStorage.getItem("returnUrl");
-//     if (stored) {
-//       setReturnUrl(stored);
-//       sessionStorage.removeItem("returnUrl");
-//     }
-//   }, []);
-
   return (
     <AuthenticateWithRedirectCallback />
   );
